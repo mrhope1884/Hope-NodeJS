686914101fadd75122f7032d5bd3ad43ddf61d85
mrhope1884 <vuvietquan1884@gmail.com>
2026-04-22 23:49:39 +0700

    move server.js to src folder and add views/sample.ejs

--- src/server.js
+++ src/server.js
@@ -2,12 +2,16 @@ const express = require('express')
 const app = express();
 const port = 8080;
 
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
     res.send('check ABC')
 })
 
 app.get('/HopeIT', (req, res) => {
-    res.send('<h1>Hope IT<h1>')
+    // res.send('<h1>Hope IT<h1>')
+    res.render('sample.ejs')
 })
 
 app.listen(port, () =>{
